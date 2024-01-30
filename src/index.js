1ec518dc0bd224bbc5037a0fe9e38653bf0e2e07
amaanraaj <amandwivedi00@gmail.com>
2024-01-30 23:32:20 +0530

    basic rabbitmq setup done for consumption of data from queue

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const amqplib = require('amqplib')
 
 const app = express();
 // const mailSender = require('./config/email-config')
+
+//rabbit mq implementation
+async function connectQueue(){
+    try {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        await channel.assertQueue("notification-queue");
+        channel.consume("notification-queue",(data)=>{
+            console.log(`${Buffer.from(data.content)}`);
+            channel.ack(data)
+        });
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -20,4 +37,6 @@ app.listen(ServerConfig.PORT, async () => {
     //     text: 'yes its working '
     // });
     // console.log( res);
+    await connectQueue();
+    console.log("Queue is up");
 });
